ab096922d392bf7a060bc676a9fb40d21fa6415d
lawrenceagles <lawrenceagles@gmail.com>
2021-05-03 10:03:17 +0100

    configured babel to use import/export

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,8 +1,16 @@
+const user = require('../models/user');
+
 const resolvers = {
 	Query: {
 		add: async (_, obj) => {
+			console.log('obj', obj);
 			const { x, y } = obj;
 			return x + y;
+		},
+
+		user(_, obj) {
+			console.log('ctx', obj);
+			return 'Lawrence Eagles';
 		}
 	}
 };
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,6 +1,7 @@
 const schema = `
   type Query {
     add(x: Int, y: Int): Int
+    user: String! 
   }
 `;
 
